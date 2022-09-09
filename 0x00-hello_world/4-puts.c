b6b35e54cb7a1892bac4b9fc8880b384d5eb50c4
Kennynyamai <kennynyamai73@gmail.com>
2022-09-08 17:09:15 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - First function to run
  *
- *   * main - First function to run
- *
- *     *
- *
- *       * Return: 0 for success
- *
- *         */
-
+ * Return: 0 for success
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
