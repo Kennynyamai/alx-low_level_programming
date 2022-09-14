aaf3a873c80c65dac9a1eda7a2914fd7bd26367b
Kennynyamai <kennynyamai73@gmail.com>
2022-09-14 15:56:59 -0700

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,15 @@
  *
  * Return: 0 for success
  */
+
 int main(void)
 {
 	char ptchar[9] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
-	{
 		_putchar(ptchar[i]);
-	}
 	_putchar('\n');
+
 	return (0);
 }
